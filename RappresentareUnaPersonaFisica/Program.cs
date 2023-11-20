58800c2b49ad252cff0d93f6dd5ef737c01136b8
MirkoColo <colombomirko06@gmail.com>
2023-11-20 09:40:42 +0100

    Aggiungere i file di progetto.

---
+++ RappresentareUnaPersonaFisica/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RappresentareUnaPersonaFisica
+{
+    class DatiAnagrafici
+    {
+        protected int _codice;
+        protected string _nome;
+        protected string _cognome;
+        protected bool _registrato;
+
+        public DatiAnagrafici() 
+        {
+            _codice = 101;
+            _nome = "Fabio";
+            _cognome = "Rovazzi";
+            _registrato = false;
+        } 
+        public void Registra()
+        {
+            Console.WriteLine($"Registrazione avvenuta con successo");
+            _registrato = true;
+        }
+
+        public void Stampa()
+        {
+            Console.WriteLine($"Nome: {_nome}\nCognome: {_cognome}\nCodice: {_codice}\nRegistrato: {(_registrato ? "Sì" : "No")}");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatiAnagrafici datiAnagrafici = new DatiAnagrafici();
+            datiAnagrafici.Stampa();
+            datiAnagrafici.Registra();
+            datiAnagrafici.Stampa();
+        }
+    }
+}
